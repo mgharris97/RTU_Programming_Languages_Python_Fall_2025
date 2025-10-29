f837c1c7aa988a075687aebb05c95eaf692a6267
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2025-10-30 01:30:50 +0200

    Finished 1_3

--- src/week1/python_lab1_task3.py
+++ src/week1/python_lab1_task3.py
@@ -7,12 +7,24 @@ Write a function `analyze_sentence(text)` that returns:
 3. whether it contains the word "Python" (case-insensitive)
 Return results as a tuple and print summary in main.
 """
+##iterate over each char
+##
 
 def analyze_sentence(text):
     """Return length, word count, and whether 'Python' appears in text."""
     # TODO: implement function logic
-    pass
+    text_length = len(text)
+    word_count = len(text.split())
+    is_python = "python" in text.lower()
+    ##tuple
+    return (text_length, word_count, is_python)
 
 if __name__ == "__main__":
     # TODO: read sentence from input, call function, and print results
-    pass
+    sentence = input("Enter text: ")
+    results = analyze_sentence(sentence)
+
+    print(f"Total chars: {results[0]}")
+    print(f"Word count: {results[1]}")
+    print(f"Contains 'Python': {results[2]}")
+
