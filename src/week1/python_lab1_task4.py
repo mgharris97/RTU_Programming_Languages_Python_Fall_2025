7fab4c7e225ea61c25df507d8ad90847c88832b4
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2025-10-30 02:42:50 +0200

    Finished all lab1

--- src/week1/python_lab1_task4.py
+++ src/week1/python_lab1_task4.py
@@ -12,27 +12,42 @@ Use helper functions:
 - analyze_text(text)
 Print formatted summary in main.
 """
+import re
 
-def count_characters(text):
+def count_characters(text: str) -> int: #retun type int
     """Count non-space characters in a string."""
     # TODO: implement
-    pass
+    char_count = len(text.replace(" ", ""))  ##remove all white spaces then count the chars
+    return char_count
 
-def count_words(text):
+def count_words(text: str) -> int:
     """Count number of words in a string."""
     # TODO: implement
-    pass
+    word_count = len(text.split()) ##make a list of words split by white spaces where each word is an item in the list
+    return word_count
 
-def extract_numbers(text):
+def extract_numbers(text: str) -> list[int]:
     """Return list of integers found in text."""
     # TODO: implement
-    pass
+    nums = re.findall(r"\d+", text)
+    nums_list = []
+    for n in nums:
+        nums_list.append(int(n))
+    return nums_list
 
-def analyze_text(text):
+
+def analyze_text(text: str):
     """Perform text-based arithmetic analysis."""
     # TODO: call helper functions and compute total, average, etc.
-    pass
+    print("Summary: ")
+    print(f"Total word count: {count_words(text)}")
+    print(f"Character count excluding spaces: {count_characters(text)}")
+    print(f"Numbers in text: {extract_numbers(text)}")
+
+
+
 
 if __name__ == "__main__":
     # TODO: read input, call analyze_text(), and print results
-    pass
+    text = input("Input text: ")
+    analyze_text(text)
