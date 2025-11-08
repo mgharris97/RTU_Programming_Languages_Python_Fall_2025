9bab3f39410c16ef74f9971338938e728616084d
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2025-11-08 17:56:40 +0200

    completed week 2 task 1

--- src/week2/python_lab2_task1.py
+++ src/week2/python_lab2_task1.py
@@ -16,16 +16,16 @@ Instructions:
 """
 
 # TODO: Create the datasets - up to you to fill in the data
-temperatures = []
-city_population = {}
+temperatures = [64, 60, 56, 42, 40]
+city_population = {"Riga": 632614, "New York": 8419600, "Berlin": 3769000, "London": 8982000, "Tokyo": 13929286}
 
 # TODO: Compute aggregates
-average_temperature = 0
-largest_city = ""
-largest_population = 0
-total_population = 0
+average_temperature = sum(temperatures) / len(temperatures)
+largest_city = max(city_population, key=city_population.get)
+largest_population = max(city_population.values())
+total_population = sum(city_population.values())
 
 # TODO: Print results
-print("Average temperature:", average_temperature)
+print(f"Average temperature: {average_temperature} degrees fahrenheit")
 print("Largest city:", largest_city, "-", largest_population)
 print("Total population:", total_population)
