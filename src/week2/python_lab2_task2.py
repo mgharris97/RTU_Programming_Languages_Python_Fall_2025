cab799f88c2071d5e23878822df2f466a2008d17
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2025-11-09 01:03:19 +0200

    Finished task2

--- src/week2/python_lab2_task2.py
+++ src/week2/python_lab2_task2.py
@@ -15,15 +15,31 @@ Given the list:
 4. Create a dictionary `cubes` mapping each number to its cube.
 5. Print all results.
 """
+import random
 
 # Fill in your own numbers or generate 10 random integers
 numbers = []
+for i in range(10):
+    numbers.append(random.randint(1,10))
 
 # TODO: Implement comprehensions
 squares = []
+for i in numbers:
+    squares.append(i**2)
+
 positives = []
-even_squares = set()
-cubes = {}
+for i in numbers:
+    if i > 0:
+        positives.append(i)
+
+even_squares = []
+for i in numbers:
+    if i%2==0:
+        even_squares.append(i**2)
+
+cubes = []
+for i in numbers:
+    cubes.append(i**3)
 
 # TODO: Print results
 print("Squares:", squares)
